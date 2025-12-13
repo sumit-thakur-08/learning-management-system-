d5fe4e0ba6e9cc1e176d5a873f160e689e4abc29
Sumit Thakur <sumit.thakur.official.2004@gmail.com>
2025-12-13 23:05:33 +0530

    Protected Routes (user/admin)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { Login, Signup } from "./pages/auth";
 import { Navbar } from "./components";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+import UserDashboard from "./pages/user/UserDashboard";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 
 function App() {
   return (
@@ -12,6 +16,26 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+
+        {/* USER ROUTE */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute role="user">
+              <UserDashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* ADMIN ROUTE */}
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute role="admin">
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
